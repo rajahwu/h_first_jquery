f464e6187cad93e3374acb25c8f538f41b567407
rajahwu <vmradford@gmail.com>
2023-01-19 20:37:46 -0500

    complete chapter seven project

--- monster_mashup/scripts/my_script.js
+++ monster_mashup/scripts/my_script.js
@@ -1,41 +1,78 @@
 
 $(document).ready(function(){
-    
-    const clicks = { headclix: 0, eyeclix: 0, noseclick: 0, mouthclick: 0 };
-        lightning_one(4000);
-        lightning_two(5000);
-        lightning_three(7000);
 
-    $("#head").click(function(){
-        clicks.headclix < 9 ? clicks.headclix += 1 : clicks.headclix = 0;
-        $(this).animate({left: -(clicks.headclix * 367)})
-    })
-    $("#eyes").click(function(){
-        clicks.eyeclix < 9 ? clicks.eyeclix += 1 : clicks.eyeclix = 0;
-        $(this).animate({left: -(clicks.eyeclix * 367)})
-    })
-    $("#nose").click(function(){
-        clicks.noseclick < 9 ? clicks.noseclick += 1 : clicks.noseclick = 0;
-        $(this).animate({left: -(clicks.noseclick * 367)})
-    })
-    $("#mouth").click(function(){
-        clicks.mouthclick < 9 ? clicks.mouthclick += 1 : clicks.mouthclick = 0;
-        $(this).animate({left: -(clicks.mouthclick * 367)})
-    })
+  let int1, int2, int3;
+  goLightning();
+  window.onblur = stopLighting;
+  window.onfocus = goLightning;
+  
+  const clicks = { headclix: 0, eyesclix: 0, noseclick: 0, mouthclick: 0 };
+       
+  $("#head").click(function(){
+      slideHeadLeft(clicks, "headclix", $(this));
+  })
+  $("#eyes").click(function(){
+     slideHeadLeft(clicks, "eyeclix", $(this));
+  })
+  $("#nose").click(function(){
+     slideHeadLeft(clicks, "noseclick", $(this));
+  })
+  $("#mouth").click(function(){
+    slideHeadLeft(clicks, "mouthclick", $(this));
+  })
 
-  function lightning_one(t) {
+  const m = 10;
+  $("#btnRandom").click(randomize);
+  $("#btnReset").click(reset);
+
+  function randomize() {
+    $(".face").each(function() {
+     const key = `${this.id}clix`;
+     clicks[key] = getRandom(m);
+     slideHeadLeft(clicks, key, $(this));
+    })
+  }
+
+  function reset() {
+    $(".face").each(function() {
+      $(this).animate({left: "0px"},500);
+    })
+    Object.keys(clicks).forEach(key => clicks[key] = 0);
+    console.log(clicks);
+  }
+
+  function getRandom(num) {
+    return Math.floor(Math.random() * num);
+  }
+
+  function slideHeadLeft(obj, key, $wraper) {
+    obj[key] < 9 ? obj[key] += 1 : obj[key] = 0;
+    $wraper.animate({left: -(obj[key] * 367)});
+  }
+
+  function goLightning() {
+    int1 = setInterval(lightning_one,4000);
+    int2 = setInterval(lightning_two,5000);
+    int3 = setInterval(lightning_three, 7000);
+  }
+
+  function stopLighting() {
+    clearInterval(int1);
+    clearInterval(int2);
+    clearInterval(int3);
+  }
+
+  function lightning_one() {
     $("#container #lightning1").fadeIn(250).fadeOut(250);
-    return setTimeout(lightning_one, t)
   }
 
-  function lightning_two(t) {
+  function lightning_two() {
     $("#container #lightning2").fadeIn(250).fadeOut(250);
-    return setTimeout(lightning_two, t)
   }
 
-  function lightning_three(t) {
+  function lightning_three() {
     $("#container #lightning3").fadeIn(250).fadeOut(250);
-    return setTimeout(lightning_three, t)
   }
+
 })
 
